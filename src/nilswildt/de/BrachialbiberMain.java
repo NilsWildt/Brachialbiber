e29fd446b218d395138afb666e95aebdf84187e0
Gainerflash <markussschmidgall@gmx.de>
2014-11-02 17:27:44 +0100

    Update BrachialbiberMain.java

--- src/nilswildt/de/BrachialbiberMain.java
+++ src/nilswildt/de/BrachialbiberMain.java
@@ -5,22 +5,36 @@ import lejos.hardware.Sound;
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.SensorPort;
+import lejos.utility.Delay;
 
 public class BrachialbiberMain {
 	public static void main(String[] args) {
+		long time;
+		
 		Brachialbiber biber = new Brachialbiber(SensorPort.S1, SensorPort.S2,
 				MotorPort.B, MotorPort.A,MotorPort.D);
 		Sound.beep();
-		Brachialbiber.printer("Press  enter to continue");
+		Brachialbiber.printer("Press enter to continue");
 		Button.waitForAnyPress();
 		biber.gyCo.initalizeGyroController();
-		biber.lineCo.initializeLineController();
-		biber.lineCo.startMotor();
+		biber.lineFollower.initFollower();
+		biber.lineFollower.initMotor();
 		LCD.clear();
-		Brachialbiber.printer("I'm working...");
-		while (Button.ESCAPE.isUp()) {
-			biber.lineCo.drive();
-			biber.gyCo.setMotion();
+		Sound.beep();
+		
+		while (Button.ESCAPE.isUp() || biber.lineFollower.isBlue()){
+			/*float[] s = biber.lineFollower.fetchAverageSample();
+			LCD.clear();
+			LCD.drawString("Red:" , 0, 0);
+			LCD.drawInt((int) (s[0]*100.0f), 5, 0);
+			LCD.drawString("Green:" , 0, 1);
+			LCD.drawInt((int) (s[1]*100.0f), 7, 1);
+			LCD.drawString("Blue:" , 0, 2);
+			LCD.drawInt((int) (s[2]*100.0f), 6, 2);
+			Delay.msDelay(200);*/
+			biber.lineFollower.drive();
+			//biber.gyCo.setMotion();
 		}
+		
 	}
 }
