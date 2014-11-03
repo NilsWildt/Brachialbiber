f3d66936ab10cd7d8ffe3574242851cd1a22fc14
Gainerflash <markussschmidgall@gmx.de>
2014-11-03 23:38:35 +0100

    Update BrachialbiberMain.java

--- src/nilswildt/de/BrachialbiberMain.java
+++ src/nilswildt/de/BrachialbiberMain.java
@@ -10,12 +10,12 @@ import lejos.utility.Delay;
 public class BrachialbiberMain {
 	public static void main(String[] args) {
 		boolean isBlue = false;
-		boolean onlyOnce = false;
+		int onlyOnce = 0;
 		double i = 8.0;
 		
 		Brachialbiber biber = new Brachialbiber(SensorPort.S1, SensorPort.S2,
 				MotorPort.B, MotorPort.A,MotorPort.D);
-		Sound.beep();
+		//Sound.beep();
 		Brachialbiber.printer("Press enter to continue");
 		Button.waitForAnyPress();
 		Delay.msDelay(1000);
@@ -23,35 +23,19 @@ public class BrachialbiberMain {
 		Delay.msDelay(200);
 		//biber.lineFollower.initFollower();
 		//biber.lineFollower.initMotor();
+		//LCD.clear();
+		//Sound.beep();
+		//Delay.msDelay(200);
 		LCD.clear();
-		Sound.beep();
-		Delay.msDelay(200);
-		LCD.clear();
-		//LCD.drawString("Red:" , 0, 0);
-		//LCD.drawString("Green:" , 0, 1);
-		//LCD.drawString("Blue:" , 0, 2);
-		while (Button.ESCAPE.isUp()){// && !isBlue){
-			if(Button.UP.isDown()){
-				i += 0.1;
-				biber.gyCo.setKP(i);
-			}
-			if(Button.DOWN.isDown()){
-				i -= 0.1;
-				biber.gyCo.setKP(i);
-			}
-			/*LCD.clear();
-			float[] s = biber.lineFollower.fetchAverageSample();
-			LCD.drawString("                 ", 5, 0);
-			LCD.drawString("                 ", 7, 1);
-			LCD.drawString("                 ", 6, 2);
-			LCD.drawInt((int) (s[0]*100.0f), 5, 0);
-			LCD.drawInt((int) (s[1]*100.0f), 7, 1);
-			LCD.drawInt((int) (s[2]*100.0f), 6, 2);
-			Delay.msDelay(100);*/
-			//biber.lineFollower.drive();
+		//-------------------------------------------------------------
+		while (Button.ESCAPE.isUp()){// && onlyOnce < 2){
+			/*biber.lineFollower.drive();
 			isBlue = biber.lineFollower.isBlue();
-			if(isBlue) Sound.beep();//onlyOnce = true;
-			if(onlyOnce){}
+			if(isBlue){
+				Sound.beep();
+				onlyOnce++;
+			}
+			*/
 			biber.gyCo.setMotion();
 		}	
 	}
