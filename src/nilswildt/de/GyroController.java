772d008f38f102c014bcaf8615bd6b4bd4532448
Gainerflash <markussschmidgall@gmx.de>
2014-11-03 23:39:26 +0100

    Update GyroController.java

--- src/nilswildt/de/GyroController.java
+++ src/nilswildt/de/GyroController.java
@@ -12,7 +12,7 @@ public class GyroController {
 	NXTMotor motor;
 
 	// Feste Werte des Controllers
-	private double Kp = 8; // ergibt sich empirisch, Verwendung in setMotion()!
+	private double Kp = 1.5; // ergibt sich empirisch, Verwendung in setMotion()!
 	private final double BLOCK_DOWN = -0.9;
 	private int newPower = 0; // from setMotion
 	private double angleVelocity = 0.0;
@@ -38,7 +38,7 @@ public class GyroController {
 	 * Initialsiert die Startwerte des Gyrosensors (Wert wird auf 0 gesetzt (hopefully XD))
 	 */
 	public void initalizeGyroController() {
-		Brachialbiber.printer("Gyrosenesor wird initialisiert.");
+		Brachialbiber.printer("Gyrosensor wird initialisiert.");
 		Delay.msDelay(500);
 		LCD.clear();
 		sensor.reset(); // Das is iwie n bissl unsave, es setzt den Gyro in Mode 4, aber ich hab kein Plan, was der
@@ -50,10 +50,15 @@ public class GyroController {
 	/**
 	 * @return Momentane Winkelgeschwindigkeit des Gyros TODO Filter? Median? Mittelwert?
 	 */
-	public float getAngleVelocity() {
-		 float[] angleVelocity = new float[1];
-		 provider.fetchSample(angleVelocity, 0);
-		 return angleVelocity[0];
+	public float getAngleVelocity(int times) {
+		float sum = 0f;
+		float[] angleVelocity = new float[1];
+		for(int i=0; i<times; i++){
+			provider.fetchSample(angleVelocity, 0);
+			sum += angleVelocity[0];
+		}
+		provider.fetchSample(angleVelocity, 0);
+		return sum;
 	}
 
 	/**
@@ -63,7 +68,7 @@ public class GyroController {
 	 *            Momentane Winkelgeschwindigkeit des Gyrosensors
 	 */
 	public void setMotion() {
-		angleVelocity = (double) getAngleVelocity();
+		angleVelocity = (double) getAngleVelocity(3);
 		//System.out.println(Math.signum(angleVelocity));
 		if (angleVelocity < 0.0) {
 			motor.backward();
@@ -76,5 +81,6 @@ public class GyroController {
 		newPower = (int) (Kp * angleVelocity);
 		newPower = Math.min(newPower, 100);
 		motor.setPower(newPower);
+		System.out.println(newPower);
 	}
 }
