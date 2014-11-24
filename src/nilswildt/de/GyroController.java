f3b33c88857ac6fb4c2296fd23a38f405f1af765
Gainerflash <markussschmidgall@gmx.de>
2014-11-24 16:33:19 +0100

    Update GyroController.java

--- src/nilswildt/de/GyroController.java
+++ src/nilswildt/de/GyroController.java
@@ -1,23 +1,16 @@
-package nilswildt.de;
-
-import java.io.IOException;
-
-import lejos.hardware.lcd.LCD;
-import lejos.hardware.motor.NXTMotor;
-import lejos.hardware.sensor.EV3GyroSensor;
-import lejos.robotics.SampleProvider;
-import lejos.utility.Delay;
 
 public class GyroController {
 	EV3GyroSensor sensor;
+	EV3TouchSensor touch;
 	NXTMotor motor;
 
 	// Feste Werte des Controllers
 	private double Kp = 3.9; // ergibt sich empirisch, Verwendung in setMotion()!
-	private final double BLOCK_DOWN = 1; // -0.9
+	private final double BLOCK_DOWN = 0.9; // -0.9
 	private double newPower = 0; // from setMotion
 	private double angleVelocity = 0.0;
 	private double sumPower = 0.0;
+	private float[] isTouched = new float[1];
 	int cnt = 0;
 	WriteFile write;
 	private SampleProvider provider;
@@ -28,8 +21,9 @@ public class GyroController {
 	 * @author Markus Schmidgall
 	 * @throws IOException
 	 */
-	public GyroController(EV3GyroSensor gyroSensor, NXTMotor nxtMotor) {
+	public GyroController(EV3GyroSensor gyroSensor, EV3TouchSensor touchSensor, NXTMotor nxtMotor) {
 		sensor = gyroSensor;
+		touch = touchSensor;
 		motor = nxtMotor;
 		provider = sensor.getRateMode();
 		write = new WriteFile();
@@ -40,8 +34,13 @@ public class GyroController {
 	 * Initialsiert die Startwerte des Gyrosensors (Wert wird auf 0 gesetzt (hopefully XD))
 	 */
 	public void initalizeGyroController() {
+		Brachialbiber.printer("Press touch to continue");
+		do{
+			touch.fetchSample(isTouched, 0);
+		}while(isTouched[0] != 1f);
+		LCD.clear();
 		Brachialbiber.printer("Gyrosensor wird initialisiert.");
-		Delay.msDelay(1000);
+		Delay.msDelay(3000);
 		LCD.clear();
 		sensor.reset();
 		Delay.msDelay(1000);
@@ -58,13 +57,13 @@ public class GyroController {
 	}
 
 	/**
-	 * Diese Methode korrigiert den Winkel des Eihalters, so dass er mÃ¶glichst gerade bleibt
+	 * Diese Methode korrigiert den Winkel des Eihalters, so dass er mÃƒÂ¶glichst gerade bleibt
 	 * 
 	 * @param angle
 	 *            Momentane Winkelgeschwindigkeit des Gyrosensors
 	 */
 	public void setMotion() {
-		angleVelocity = (double) getAngleVelocity(); // �ber wie viel gemittelt wird;
+		angleVelocity = (double) getAngleVelocity(); // Über wie viel gemittelt wird;
 		//write.writeToFile(angleVelocity); // Log angleVelocity
 		// Calculate Power
 		newPower = (Kp * angleVelocity);
