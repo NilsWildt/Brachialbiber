df6f00946abf458e14b5beacf02951e9bef53ac2
Gainerflash <markussschmidgall@gmx.de>
2014-11-27 21:49:37 +0100

    Update BrachialbiberMain.java

--- src/nilswildt/de/BrachialbiberMain.java
+++ src/nilswildt/de/BrachialbiberMain.java
@@ -19,8 +19,8 @@ public class BrachialbiberMain {
 		GyroController controller = biber.gyCo;
 		
 		//Initialisierungen
-		Delay.msDelay(1000);
-		controller.initalizeGyroController();
+		//Delay.msDelay(1000);
+		//controller.initalizeGyroController();
 		Delay.msDelay(200);
 		follower.initFollower();
 		follower.initMotor();
@@ -28,50 +28,20 @@ public class BrachialbiberMain {
 		LCD.clear();
 		// -------------------------------------------------------------
 
-		/*
-		 * Blue stuff: Beachte: KP WERTE NICHT VERGESSEN Wie viele count?
-		 */
-		int blueCount = 0;
-		boolean blueFlag = false;
-		// Sound.setVolume(5); // Damit es nicht so laut Piept.
-		long startTime = System.currentTimeMillis();
-		long elapsedTime = 0L;
-
+		
 		while (Button.ESCAPE.isUp()) {
-			// TODO Werte von Klassen abgreifen, um nicht jedesmal hier hardcoden zu müssen.
 			follower.drive();
 			//controller.setMotion();
-			
-			/*if (false) { // biber.lineFollower.isBlue()
-				++blueCount;
-				elapsedTime = (new Date()).getTime() - startTime;
-				if (blueCount >= 1 && !blueFlag && elapsedTime >= (3 * 1000)) {
-					System.out.println(elapsedTime);
-					Sound.beep();
-					blueFlag = true;
-					blueCount = 0;
-					biber.gyCo.setGyroKP(3.8); // davor auf 0...
-					biber.lineFollower.setKP(2.0); // KP auf dem Hügel runter setzen!
-					startTime = System.currentTimeMillis();
-				} else if (blueCount >= 1  && blueFlag
-						&& elapsedTime >= (3 * 1000)) {
-					Sound.beep();
-					blueCount = 0;
-					blueFlag = false;
-					startTime = System.currentTimeMillis();
-					biber.gyCo.setGyroKP(0); // davor auf 0...
-					biber.lineFollower.setKP(5.2);
-				}
-				*/
 		}
-		System.exit(0);
+		
+		/*
 		// Play Music and stop!
 		biber.gyCo.interruptMotion();
 		biber.lineFollower.stopMotor();
 		File musicFile = new File("quali2.wav");
 		Sound.setVolume(100);
 		Sound.playSample(musicFile, 100);
-		Button.waitForAnyPress();
+		Button.waitForAnyPress();*/
 		System.exit(0);
 	}
 }
